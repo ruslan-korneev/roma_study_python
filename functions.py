7113a7e6219c95c29566e47f7befb04f0bd45252
Ruslan <kornerus@fjfi.cvut.cz>
2022-02-18 21:14:55 +0300

    lesson 2 about functions

---
+++ functions.py
@@ -0,0 +1,21 @@
+from typing import Optional, Union
+
+
+def summa(a: Optional[int], b: int) -> Union[int, str]:
+    """ This function summ `a` and `b` and return number """
+    if not a:
+        return "Cool!"
+    return a + b
+
+a = 5
+b = 10
+
+c = summa(range(10), b)
+print(c)
+c = summa(4, summa(a, b))
+print(c)
+
+
+# 1. когда ты видишь, что ты несколько раз пишешь одно и тоже, создай функцию
+# 2. пиши документации к функцие
+# 3. пиши аннотации к функцие
